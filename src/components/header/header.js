b6563160e487809dc8da0e25d92b8e1eddf62236
Christine Lao <christy_11_12@yahoo.com>
2019-11-07 22:50:54 -0800

    Style the header on mobile devices (<= 768px)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -30,7 +30,12 @@ const Header = () => {
   )
 
   return (
-    <header>
+    <header class="header">
+      <span class="icon-bars header__icon-menu"></span>
+      <div class="left-sidebar">
+        <p class="left-sidebar__text left-sidebar__text--name"> Christine Lao </p>
+        <p class="left-sidebar__text left-sidebar__text--email">christinelao3o@gmail.com</p>
+      </div>
       <img className="header__logo" src="http://via.placeholder.com/75x35" />
       <nav
         className="nav-menu"
@@ -39,6 +44,15 @@ const Header = () => {
           <NavMenuItem menuItem={edge.node} key={edge.node.heading} />
         ) }
       </nav>
+      <div class="links-to-additional-information">
+        <a href="" class="links-to-additional-information__link links-to-additional-information__link--email">
+          <span class="icon-envelope-o"></span>
+        </a>
+        <a href="" class="links-to-additional-information__link links-to-additional-information__linkk--linked-in">
+          <span class="icon-linkedin-square"></span>
+        </a>
+        <a href="" class="links-to-additional-information__link links-to-additional-information__link--resume">Resume</a>
+      </div>
     </header>
   )
 }
