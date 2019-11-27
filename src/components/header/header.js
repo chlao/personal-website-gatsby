f2e35203b5bbef02f804dc515933fc72f262b24c
Christine Lao <christy_11_12@yahoo.com>
2019-11-26 22:04:29 -0800

    Move the nav menu inside the header out into its own component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,9 @@
 import PropTypes from "prop-types"
 import React, { useState } from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import LeftSideBar from '../leftSideBar/leftSideBar'
+import NavMenu from '../navMenu/navMenu'
 import LinksToAdditionalInformation from '../linksToAdditionalInformation/linksToAdditionalInformation'
-import NavMenuItem from "./NavMenuItem"
 
 import "./header.scss"
 
@@ -13,38 +12,12 @@ require("typeface-open-sans")
 const Header = () => {
   const [open, setOpen] = useState(false);
 
-  const data = useStaticQuery(
-    graphql`
-      query {
-        allContentfulNavigationMenuItem(
-          sort: {
-            fields: position
-             order: ASC
-            }
-          ){
-          edges {
-            node {
-              heading
-              link
-            }
-          }
-        }
-      }
-    `
-  )
-
   return (
     <header className={`header${open ? " header--open" : ""}`} >
-      <span className={`header__icon-menu ${open ? "icon-close" : "icon-bars"}`} onClick={() => { console.log(open); setOpen(!open)}}></span>
+      <span className={`header__icon-menu ${open ? "icon-close" : "icon-bars"}`} onClick={() => { setOpen(!open)}}></span>
       <LeftSideBar headerOpenState={open}/>
       <img className="header__logo" src="http://via.placeholder.com/75x35" />
-      <nav
-        className={`nav-menu${open ? " nav-menu--open" : ""}`}
-      >
-        { data.allContentfulNavigationMenuItem.edges.map((edge) =>
-          <NavMenuItem menuItem={edge.node} key={edge.node.heading} />
-        ) }
-      </nav>
+      <NavMenu headerOpenState={open}/>
       <LinksToAdditionalInformation headerOpenState={open}/>
     </header>
   )
---
+++ src/components/navMenu/navMenu.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import NavMenuItem from "./navMenuItem"
+
+import "./navMenu.scss"
+
+const NavMenu = ({ headerOpenState }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulNavigationMenuItem(
+          sort: {
+            fields: position
+             order: ASC
+            }
+          ){
+          edges {
+            node {
+              heading
+              link
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <nav
+      className={`nav-menu${headerOpenState ? " nav-menu--open" : ""}`}
+    >
+      { data.allContentfulNavigationMenuItem.edges.map((edge) =>
+        <NavMenuItem menuItem={edge.node} key={edge.node.heading} />
+      ) }
+    </nav>
+  )
+}
+
+export default NavMenu
