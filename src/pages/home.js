c73490dc7eec5a61fa9f1f010b6ba7daf120be43
Christine Lao <christy_11_12@yahoo.com>
2019-11-30 22:58:45 -0800

    Style the home content for mobile and screen sizes in between
    Redo the home content centering as the div cannot be positioned absolute.
    There will be elements coming after the div, so it shouldn't be taken out of the flow.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,16 @@
- import React from "react"
+import React from "react"
 
-
- import "src/styles/home.scss"
+import "src/styles/home.scss"
 
  const Home = () => {
    return (
      <div className="home">
-       <p className="home__overline">Software Engineer</p>
-       <h1 className="home__heading">Christine Lao</h1>
-       <p className="home__subheading">Constantly striving to build elegant, flexible solutions that are beautiful inside and out </p>
-       <p className="home__underline">WEB. DESIGN. VERSATILITY</p>
+      <div className="home__inner">
+         <p className="home__overline">Software Engineer</p>
+         <h1 className="home__heading">Christine Lao</h1>
+         <p className="home__subheading">Constantly striving to build elegant, flexible solutions that are beautiful inside and out </p>
+         <p className="home__underline">Web. Design. Versatility</p>
+       </div>
      </div>
    )
  }
