e0a49e798513f6491da1c61da39391f85fb66805
Christine Lao <christy_11_12@yahoo.com>
2019-12-30 22:58:05 -0800

    Add the hobbies section

---
+++ src/pages/hobbies.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Hobbies = () => (
+  <section className="hobbies">
+    <div className="hobbies__outer section__outer">
+      <div className="hobbies__heading-inner">
+        <h2 className="section__heading"> Hobbies </h2>
+      </div>
+      <div className="hobbies__text-inner">
+        <div className="hobbies__text">
+        <p>fashion, baking, ceramics, food, board games, sustainability, organization, traveling</p>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Hobbies
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Home from "./home"
 import AboutMe from "./aboutMe"
 import Experience from "./experience"
 import Skills from "./skills"
+import Hobbies from "./hobbies"
 import Logo from '../components/logo'
 
 require('typeface-open-sans')
