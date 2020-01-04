de5c5685205972c43ca751a639bbf79faf3f9788
Christine Lao <christy_11_12@yahoo.com>
2020-01-04 13:57:27 -0800

    Update the site metadata title and description and the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Christine Lao, Personal Website',
+    description: `A software engineer that's constantly striving to build
+      elegant, flexible solutions that are beautiful inside and out.`,
+    author: `@chlao`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       }
     },
     {
