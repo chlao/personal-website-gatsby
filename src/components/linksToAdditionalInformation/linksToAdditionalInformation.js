42b2d2bad0e8324b98d928f11804a09a05b01976
Christine Lao <christy_11_12@yahoo.com>
2020-01-07 22:13:13 -0800

    Obtain the resume PDF URL from the CMS via a GraphQL query

--- src/components/linksToAdditionalInformation/linksToAdditionalInformation.js
+++ src/components/linksToAdditionalInformation/linksToAdditionalInformation.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import "./linksToAdditionalInformation.scss"
 
 const LinksToAdditionalInformation = ({ headerOpenState, isDesktop }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulResume {
+          edges {
+            node {
+              file {
+                file {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const resumeUrl = data.allContentfulResume.edges[0].node.file.file.url;
+
   const headerOpenClass = headerOpenState ? " links-to-additional-information--md-open" : ""
   const isDesktopClass = isDesktop ? " links-to-additional-information--lg" : " links-to-additional-information--md"
 
@@ -14,7 +35,7 @@ const LinksToAdditionalInformation = ({ headerOpenState, isDesktop }) => {
       <a href="https://www.linkedin.com/in/christinelao3o/" className="links-to-additional-information__link links-to-additional-information__link--linked-in">
         <span className="icon-linkedin-square"></span>
       </a>
-      <a href="/Resume.pdf" target="_blank" className="links-to-additional-information__link links-to-additional-information__link--resume">Resume</a>
+      <a href={resumeUrl} target="_blank" className="links-to-additional-information__link links-to-additional-information__link--resume">Resume</a>
     </div>
   )
 }
