f44a3d013cdc805e0cb7b207db305150944fe145
Christine Lao <christy_11_12@yahoo.com>
2020-01-22 20:31:25 -0800

    Query for the home content from the Contentful CMS

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,36 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import "src/styles/home.scss"
 
  const Home = () => {
+   const data = useStaticQuery(
+     graphql`
+      query {
+        allContentfulHome {
+          edges {
+            node {
+              subheading
+              underline
+              overline
+              heading
+            }
+          }
+        }
+      }
+     `
+   )
+
+   const content = data.allContentfulHome.edges[0].node
+
    return (
      <section className="home">
        <div className="home__outer section__outer">
         <div className="home__inner">
-           <p className="home__overline">Software Engineer</p>
-           <h1 className="home__heading">Christine Lao</h1>
-           <p className="home__subheading">Constantly striving to build elegant, flexible solutions that are beautiful inside and out </p>
-           <p className="home__underline">Web. Design. Versatility</p>
+           <p className="home__overline">{ content.overline }</p>
+           <h1 className="home__heading">{ content.heading }</h1>
+           <p className="home__subheading">{ content.subheading }</p>
+           <p className="home__underline">{ content.underline }</p>
          </div>
        </div>
      </section>
