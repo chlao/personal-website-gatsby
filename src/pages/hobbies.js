fb8d1ec58981049e1f95b3d2a22262f811619a35
Christine Lao <christy_11_12@yahoo.com>
2020-01-22 20:36:47 -0800

    Query for the hobbies description from the Contentful CMS

--- src/pages/hobbies.js
+++ src/pages/hobbies.js
@@ -1,18 +1,35 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Hobbies = () => (
-  <section id="hobbies" className="hobbies">
-    <div className="hobbies__outer section__outer">
-      <div className="hobbies__heading-inner">
-        <h2 className="section__heading"> Hobbies </h2>
-      </div>
-      <div className="hobbies__text-inner">
-        <div className="hobbies__text">
-        <p>fashion, baking, ceramics, food, board games, sustainability, organization, traveling</p>
+const Hobbies = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulHobbies {
+          edges {
+            node {
+              description
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <section id="hobbies" className="hobbies">
+      <div className="hobbies__outer section__outer">
+        <div className="hobbies__heading-inner">
+          <h2 className="section__heading"> Hobbies </h2>
+        </div>
+        <div className="hobbies__text-inner">
+          <div className="hobbies__text">
+          <p>{ data.allContentfulHobbies.edges[0].node.description }</p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default Hobbies
