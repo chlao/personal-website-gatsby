06b315b314d189babeb380f7ccae70943f28bb65
Christine Lao <christy_11_12@yahoo.com>
2020-01-23 21:45:06 -0800

    Query for the logo image from the Contentful CMS - Gatsby Image was not used as the logo should be a SVG and the fluid and fixed properties on the image obtained from Contentful have a null value

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
-import logo from "../images/logo.svg"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Logo = ({className}) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulLogo {
+        edges {
+          node {
+            image {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <img className={className} src={logo} alt="logo" />
+    <img className={className}
+         src={data.allContentfulLogo.edges[0].node.image.file.url}
+         alt="logo" />
   )
 }
 
